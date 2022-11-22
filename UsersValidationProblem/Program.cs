89d04c6d64c1887efd3b308746f9fa454b9f12aa
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-21 23:25:40 -0800

    Added Users Registration

--- UsersValidationProblem/Program.cs
+++ UsersValidationProblem/Program.cs
@@ -24,6 +24,11 @@ namespace UsersValidationProblem
             string phonenumber = Console.ReadLine();
             userValidation.ValidationPhoneNumber(phonenumber);
 
+            Console.WriteLine("Enter password 8 characters");
+            string password = Console.ReadLine();
+            userValidation.ValidationPassword(password);
+
+
         }
     }
 }
