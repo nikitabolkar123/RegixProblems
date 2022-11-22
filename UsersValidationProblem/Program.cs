2625f8e445a2de09e0ef961cfb9f09aa6a8c6ec0
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-21 23:10:04 -0800

    Added Users Registration

---
+++ UsersValidationProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsersValidationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            UserValidation userValidation = new UserValidation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstname = Console.ReadLine();
+            userValidation.ValidationFirstName(firstname);
+        }
+    }
+}
+        
---
+++ UsersValidationProblem/UserValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UsersValidationProblem
+{
+    public class UserValidation
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationFirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+    }
+}
+    
